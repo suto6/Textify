5dfa7145a8ef7c3809ff486330298e7a1f4463cb
suto6 <sutapacreator@gmail.com>
2024-08-07 14:06:18 +0530

    Updates

---
+++ precache-manifest.1bff35b252bc26e9333bb6189de0878b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db0aa5bfa1ceb78b8271ee7bb943afe",
+    "url": "/Textify/index.html"
+  },
+  {
+    "revision": "bb5ba5fd70cb5e94b0e0",
+    "url": "/Textify/static/css/main.b086e9e0.chunk.css"
+  },
+  {
+    "revision": "a1703f1bda2a11ab194f",
+    "url": "/Textify/static/js/2.ec19ef96.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Textify/static/js/2.ec19ef96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5770b6dbafba458b69",
+    "url": "/Textify/static/js/3.9d25297e.chunk.js"
+  },
+  {
+    "revision": "bb5ba5fd70cb5e94b0e0",
+    "url": "/Textify/static/js/main.20f4b20f.chunk.js"
+  },
+  {
+    "revision": "fd53f552c6f81b8f8805",
+    "url": "/Textify/static/js/runtime-main.26ebe0f1.js"
+  }
+]);
